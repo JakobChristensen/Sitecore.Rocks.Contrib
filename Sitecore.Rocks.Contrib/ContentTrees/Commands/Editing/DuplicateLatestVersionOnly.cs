b2507180542d9300b52539bdeb9ebfb6fab1617f
Jakob Christensen <jc@sitecore.net>
2014-03-26 11:13:05 +0100

    Added Duplicate Latest Version Only command

---
+++ Sitecore.Rocks.Contrib/ContentTrees/Commands/Editing/DuplicateLatestVersionOnly.cs
@@ -0,0 +1,101 @@
+﻿namespace Sitecore.Rocks.ContentTrees.Commands.Editing
+{
+  using System;
+  using System.Linq;
+  using Sitecore.VisualStudio;
+  using Sitecore.VisualStudio.Commands;
+  using Sitecore.VisualStudio.ContentTrees;
+  using Sitecore.VisualStudio.ContentTrees.Items;
+  using Sitecore.VisualStudio.Data;
+  using Sitecore.VisualStudio.Sites;
+
+  /// <summary>
+  /// Class CopyShortIdToClipboard.
+  /// </summary>
+  [Command]
+  public class DuplicateLatestVersionOnly : CommandBase
+  {
+    #region Constructors and Destructors
+
+    /// <summary>
+    /// </summary>
+    public DuplicateLatestVersionOnly()
+    {
+      this.Text = "Duplicate Latest Version Only";
+      this.Group = "Edit";
+      this.SortingValue = 2001;
+    }
+
+    #endregion
+
+    #region Public Methods and Operators
+
+    /// <summary>Defines the method that determines whether the command can execute in its current state.</summary>
+    /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+    /// <returns>true if this command can be executed; otherwise, false.</returns>
+    public override bool CanExecute(object parameter)
+    {
+      var context = parameter as ContentTreeContext;
+      if (context == null)
+      {
+        return false;
+      }
+
+      if (context.SelectedItems.Count() != 1)
+      {
+        return false;
+      }
+
+      var item = context.SelectedItems.FirstOrDefault() as ItemTreeViewItem;
+      if (item == null)
+      {
+        return false;
+      }
+
+      return true;
+    }
+
+    /// <summary>The execute.</summary>
+    /// <param name="parameter">The parameter.</param>
+    public override void Execute(object parameter)
+    {
+      var context = parameter as ContentTreeContext;
+      if (context == null)
+      {
+        return;
+      }
+
+      var item = context.SelectedItems.FirstOrDefault() as ItemTreeViewItem;
+      if (item == null)
+      {
+        return;
+      }
+
+      var newName = string.Format(Resources.SetName_Process_Copy_of__0_, item.Item.Name);
+      newName = AppHost.Prompt(Resources.SetName_Process_Enter_the_Name_of_the_New_Item_, Resources.SetName_Process_Duplicate, newName);
+      if (newName == null)
+      {
+        return;
+      }
+
+      Site.RequestCompleted completed = delegate(string response)
+      {
+        var newItemUri = new ItemUri(item.ItemUri.DatabaseUri, new ItemId(new Guid(response)));
+        var itemVersionUri = new ItemVersionUri(newItemUri, LanguageManager.CurrentLanguage, VisualStudio.Data.Version.Latest);
+
+        Notifications.RaiseItemDuplicated(this, newItemUri, item.ItemUri);
+
+        if (AppHost.CurrentContentTree != null)
+        {
+          AppHost.CurrentContentTree.Locate(newItemUri);
+        }
+
+        AppHost.OpenContentEditor(itemVersionUri);
+      };
+
+      item.ItemUri.Site.Execute("Items.DuplicateLatestVersionOnly", completed, item.ItemUri.DatabaseName.ToString(), item.ItemUri.ItemId.ToString(), Language.Current.ToString(), newName);
+    }
+
+    #endregion
+  }
+}
---
+++ Sitecore.Rocks.Server.Contrib/Requests/Items/DuplicateLatestVersionOnly.cs
@@ -0,0 +1,48 @@
+﻿namespace Sitecore.Rocks.Server.Requests.Items
+{
+  using Sitecore.Configuration;
+  using Sitecore.Data.Managers;
+  using Sitecore.Diagnostics;
+
+  /// <summary>
+  /// Class DuplicateLatestVersionOnly.
+  /// </summary>
+  public class DuplicateLatestVersionOnly
+  {
+    #region Public Methods and Operators
+
+    /// <summary>
+    /// Executes the specified database name.
+    /// </summary>
+    /// <param name="databaseName">Name of the database.</param>
+    /// <param name="itemId">The item identifier.</param>
+    /// <param name="languageName">Name of the language.</param>
+    /// <param name="newName">The new name.</param>
+    /// <returns>System.String.</returns>
+    public string Execute(string databaseName, string itemId, string languageName, string newName)
+    {
+      var database = Factory.GetDatabase(databaseName);
+      Assert.IsNotNull(database, "Database not found");
+
+      var language = LanguageManager.GetLanguage(languageName);
+      Assert.IsNotNull(language, "Language not found");
+
+      var item = database.GetItem(itemId, language);
+      Assert.IsNotNull(database, "Item not found");
+
+      var newItem = item.Duplicate(newName);
+
+      foreach (var version in newItem.Versions.GetVersions(true))
+      {
+        if (version.Language != language || !version.Versions.IsLatestVersion())
+        {
+          version.Versions.RemoveVersion();
+        }
+      }
+
+      return newItem.ID.ToString();
+    }
+
+    #endregion
+  }
+}
